fdd7dc3f495d2f4e1f446f4127d81e148791b10b
ILLIA121291 <illia121291@gmail.com>
2024-01-23 11:10:43 +0300

    Ready website for publication

--- src/components/App-body/squares-list/squares-list-item/stars/stars.jsx
+++ src/components/App-body/squares-list/squares-list-item/stars/stars.jsx
@@ -1,15 +1,15 @@
-import './stars.css'
+import './stars.css';
 
 function Stars() {
   return (
-    <div className='container_for_stars'>
+    <div className="container_for_stars">
       <img src="/star.svg" alt="star" />
       <img src="/star.svg" alt="star" />
       <img src="/star.svg" alt="star" />
       <img src="/star.svg" alt="star" />
       <img src="/star.svg" alt="star" />
     </div>
-  )
+  );
 }
 
-export default Stars
+export default Stars;
--- src/components/App-body/subscribe-panel/subscribe-panel.jsx
+++ src/components/App-body/subscribe-panel/subscribe-panel.jsx
@@ -1,23 +1,23 @@
-import BlueButton from '../../App-general-components/Blue-button/blue-button'
-import './subscribe-panel.css'
+import BlueButton from '../../App-general-components/Blue-button/blue-button';
+import './subscribe-panel.css';
 
-function SubscribePanel () {
+function SubscribePanel() {
   return (
-    <section className='app_body_subsribe_panel'>
+    <section className="app_body_subsribe_panel">
       <img src="/subscribe_panel.svg" alt="" />
-      <div className='app_body_subsribe_panel_part_right'>
+      <div className="app_body_subsribe_panel_part_right">
         <span>Email Newsletter</span>
         <h2>Subscribe for updates</h2>
       </div>
-      <form action="" className='app_body_subsribe_panel_part_left'>
-        <input type="text" placeholder='Name' />
-        <input type="email" placeholder='Email'/>
+      <form action="" className="app_body_subsribe_panel_part_left">
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="Email" />
         <div>
-        <BlueButton titel='Submit' />
+          <BlueButton titel="Submit" />
         </div>
       </form>
     </section>
-  )
+  );
 }
 
-export default SubscribePanel
+export default SubscribePanel;
